9bb6b55f91ba228c3ceec6550d17f6be86d0b0cb
Vlad Zubin <zoobeen1@gmail.com>
2022-05-04 18:54:11 +0300

    fix after mentor

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,41 +1,16 @@
 import Player from '@vimeo/player';
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-var throttle = require('lodash.throttle');
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-    console.log(seconds);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
+const throttle = require('lodash.throttle');
+const VCT = 'videoplayer-current-time';
+const getTime = function () {
+  player.getCurrentTime().then(seconds => {
+    localStorage.setItem(VCT, seconds);
   });
-player.on(
-  'timeupdate',
-  throttle(() => {
-    player
-      .getCurrentTime()
-      .then(seconds => {
-        // seconds = the current playback position
-        localStorage.setItem('videoplayer-current-time', seconds);
-      })
-      .catch(error => {
-        // an error occurred
-        console.log(error);
-      });
-  }, 1000),
-);
+};
+
+if (localStorage.getItem(VCT)) {
+  player.setCurrentTime(localStorage.getItem(VCT));
+}
+
+player.on('timeupdate', throttle(getTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,23 @@ const throttle = require('lodash.throttle');
 const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
+let formData = { email: '', message: '' };
+
 //on form load
-if (localStorage.getItem('email')) email.value = localStorage.getItem('email');
-if (localStorage.getItem('message')) message.value = localStorage.getItem('message');
+if (localStorage.getItem('form-data')) {
+  formData = JSON.parse(localStorage.getItem('form-data'));
+  email.value = formData.email;
+  message.value = formData.message;
+}
 
 //add event listeners
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 function onInput(e) {
-  localStorage.setItem(e.target.name, e.target.value);
+  if (e.target.name == 'email') {
+    formData.email = e.target.value;
+  } else formData.message = e.target.value;
+  localStorage.setItem('form-data', JSON.stringify(formData));
 }
 function onSubmit(e) {
   e.preventDefault();
@@ -18,10 +26,10 @@ function onSubmit(e) {
     alert('Заполните все поля формы!!!');
     return;
   }
-  console.log('email: ', localStorage.getItem('email'));
-  console.log('message: ', localStorage.getItem('message'));
-  localStorage.removeItem('email');
-  localStorage.removeItem('message');
+
+  console.log('email: ', email.value);
+  console.log('message: ', message.value);
+  localStorage.removeItem('form-data');
   email.value = '';
   message.value = '';
 }
